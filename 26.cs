0aa820fc8f0be61db9c4c43fb23cc1828ca552a6
the48 <initfi5e@gmail.com>
2023-09-29 21:54:40 +0300

    LeetCode #26

--- 26.cs
+++ 26.cs
@@ -2,6 +2,14 @@
 
 class Solution {
     public int solve(int[] nums) {
-        
+        int l = 1;
+
+        for(int r = 1; r < nums.Length; r++) {
+            if(nums[r] != nums[r - 1]) {
+                nums[l] = nums[r];
+                l += 1;
+            }
+        }
+        return l;
     }
-} 
+}
