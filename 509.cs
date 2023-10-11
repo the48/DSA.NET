c4eef2a4784a9a3489ff886bf8efe76461b124c3
the48 <initfi5e@gmail.com>
2023-10-11 22:11:07 +0300

    LeetCode #509

---
+++ 509.cs
@@ -0,0 +1,14 @@
+// LeetCode 509
+
+class Solution {
+    public int solve(int n) {
+        if(n == 0) {
+            return 0;
+        }
+
+        if(n == 1) {
+            return 1;
+        }
+        return solve(n - 1) + solve(n - 2);
+    }
+}
