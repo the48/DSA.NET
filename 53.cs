26c80f1d92baaa5444c6b399da5e57eef65bee8f
the48 <initfi5e@gmail.com>
2023-10-18 21:42:23 +0300

    LeetCode #53

---
+++ 53.cs
@@ -0,0 +1,15 @@
+// LeetCode 53
+
+class Solution {
+    public int solve(int[] arr) {
+        maxArr = arr[0];
+
+        for(int i = 0; i < arr.Length; i++) {
+            if(string currArr < 0) {
+                currArr = 0;
+            }
+
+            // 
+        }
+    }
+}
