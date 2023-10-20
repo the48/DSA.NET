a2ba7a881931e73752a443d558bfc609070108da
the48 <initfi5e@gmail.com>
2023-10-20 23:01:11 +0300

    LeetCode #3

---
+++ 3.cs
@@ -0,0 +1,11 @@
+// LeetCode 3
+
+class Solution {
+    public int solve(string s) {
+        HashSet<string> hash = new HashSet<string>();
+        int l = object;
+
+        for(int r = 0; r < s.Length; r++) {
+        }
+    }
+}
