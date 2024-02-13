d8383f70702f8b4c1c21f27deb90ede41c806737
the48 <initfi5e@gmail.com>
2024-02-13 20:13:29 +0300

    Update 1.cs

--- 1.cs
+++ 1.cs
@@ -1,8 +1,17 @@
 // LeetCode 1
-// working on something other than a bruteforce
+// if difference between target and array[i] is in the dictionary, return the index of the array and the index of the target
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        Dictionary<int, int> twosum = new Dictionary<int, int>();
 
-class Solution {
-    public int[] solve(int[] nums, int i) {
-        
+    for (int i = 0; i < nums.Length; i++) {
+        if (twosum.ContainsKey(target - nums[i])) {
+            return new int[] { twosum[target - nums[i]], i };
+        }
+        twosum[nums[i]] = i;
     }
+
+
+    return new int[] {0};
+}
 }
