f101b214e0f812cea74410fac3acfd17dac4de09
the48 <initfi5e@gmail.com>
2024-02-15 19:35:03 +0300

    Create 206.cs

---
+++ 206.cs
@@ -0,0 +1,15 @@
+class Solution {
+    public ListNode ReverseList(ListNode head) {
+        ListNode prevNode = null;
+        ListNode currNode = head;
+
+        while (currNode != null) {
+            ListNode nextNode = currNode.next;
+            currNode.next = prevNode;
+            prevNode = currNode;
+            currNode = nextNode;
+        }
+
+        return prevNode;
+    }
+}
